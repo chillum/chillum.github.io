eb0bbbb28030067f91a25ac8a91c30bf800ca1a6
Vasily Korytov <vasily.korytov@yahoo.com>
2015-02-28 00:13:22 +0200

    jade for templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp   = require('gulp'),
+    jade   = require('gulp-jade'),
     minify = require('gulp-minify-css'),
     del    = require('del');
  
 gulp.task('default', function() {
   gulp.src('src/*.css')
     .pipe(minify({keepBreaks:true}))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('.'));
+  gulp.src('src/*.jade')
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('clean', function() {
-    del(['css']);
+  del(['*css', '*.html']);
 });
