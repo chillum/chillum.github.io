d5e32400327d519da089f7aeb9433dda4e5af6bd
Vasily Korytov <vasily.korytov@yahoo.com>
2015-03-01 13:31:33 +0200

    `css` and `html` tasks are separate now

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
 var gulp   = require('gulp'),
     jade   = require('gulp-jade'),
     stylus = require('gulp-stylus'),
-    del    = require('del');
+    del    = require('del'),
+    css    = 'src/*.styl',
+    html   = 'src/*.jade';
 
-gulp.task('default', function() {
-  gulp.src('src/*.styl')
+gulp.task('default', ['css', 'html']);
+
+gulp.task('css', function() {
+  gulp.src(css)
     .pipe(stylus())
     .pipe(gulp.dest('.'));
-  gulp.src('src/*.jade')
+});
+
+gulp.task('html', function() {
+  gulp.src(html)
     .pipe(jade({pretty: true}))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['src/*.styl', 'src/*.jade'], ['default']);
+  gulp.watch([css],  ['css']);
+  gulp.watch([html], ['html']);
 });
 
 gulp.task('clean', function() {
